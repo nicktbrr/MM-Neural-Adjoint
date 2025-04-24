deebef41639dac6d9677369b13b80a15d88fa8a0
Nicholas Barsi-Rhyne <nicholasbarsi-rhyne@Nicholass-MacBook-Air.local>
2025-04-23 17:20:43 -0700

    updating for gpu

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "torch>=2.6.0",
         "numpy>=2.2.4",
         "pandas>=2.2.3",
         "tqdm>=4.67.1",
         "mlflow>=2.21.3",
         "scikit-learn>=1.4.0"
     ],
+    extras_require={
+        'cpu': ['torch>=2.6.0'],
+        'cuda': ['torch>=2.6.0+cu118'],  # for CUDA 11.8
+        'cuda12': ['torch>=2.6.0+cu121'],  # for CUDA 12.1
+    },
     author="Nicholas Barsi-Rhyne",
     author_email="nick@quantumventura.com",
     description="A neural adjoint method implementation",
